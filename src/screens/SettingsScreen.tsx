a44cc539a716da9834f6fb22f56dfeb5cac082d2
mamerida <mamerida2018@gmail.com>
2023-09-26 20:42:14 -0300

    useSafeAreaInsets

--- src/screens/SettingsScreen.tsx
+++ src/screens/SettingsScreen.tsx
@@ -1,9 +1,16 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { useSafeAreaInsets } from 'react-native-safe-area-context'
 
 const SettingsScreen = () => {
+
+  //hook que tiene los datos necesarios para mostrar el contenido 
+  // de manera segura sin usar el safearea
+  //pudiendo determinar mediante top , left, rigth o botton.
+  const insets = useSafeAreaInsets();
+
   return (
-    <View>
+    <View style={{marginTop:insets.top}}>
       <Text>SettingsScreen</Text>
     </View>
   )
