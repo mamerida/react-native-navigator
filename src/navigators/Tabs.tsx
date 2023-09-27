2d068e20b945f4a5b3ae63e02e8f20badfb5f771
mamerida <mamerida2018@gmail.com>
2023-09-27 20:10:24 -0300

    style tabs of botton navigator

--- src/navigators/Tabs.tsx
+++ src/navigators/Tabs.tsx
@@ -2,15 +2,30 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Tab1Screen from '../screens/Tab1Screen';
 import Tab2Screen from '../screens/Tab2Screen';
 import Tab3Screen from '../screens/Tab3Screen';
+import StackNavigator from './StackNavigator';
+import { colores } from '../theme/appTheme';
 
 const Tab = createBottomTabNavigator();
 
 const Tabs = ()  => {
   return (
-    <Tab.Navigator screenOptions={{ headerShown: false }} >
+    <Tab.Navigator 
+        screenOptions={{ 
+            headerShown: false,
+            tabBarActiveTintColor: colores.primary,
+            tabBarStyle: {
+              borderTopColor: colores.primary,
+              borderTopWidth: 0, // para quitarle el borde a los tabs
+              elevation: 0,
+            },
+        }}
+        sceneContainerStyle={{
+            backgroundColor: 'white',
+        }}
+    >
       <Tab.Screen name="Tab1Screen" component={Tab1Screen} />
       <Tab.Screen name="Tab2Screen" component={Tab2Screen} />
-      <Tab.Screen name="Tab3Screen" component={Tab3Screen} />
+      <Tab.Screen name="StackNavigator" component={StackNavigator} />
     </Tab.Navigator>
   );
 }
