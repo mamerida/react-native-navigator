2d2675c982a4760aca1902d26030b9da33b8e4e2
mamerida <mamerida2018@gmail.com>
2023-09-29 17:57:27 -0300

    style top navigation

--- src/navigators/TopTapNavigation.tsx
+++ src/navigators/TopTapNavigation.tsx
@@ -3,17 +3,63 @@ import { createMaterialTopTabNavigator } from '@react-navigation/material-top-ta
 import ContactsScreen from '../screens/ContactsScreen';
 import ChatsScreen from '../screens/ChatsScreen';
 import AlbumScreen from '../screens/AlbumScreen';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { colores } from '../theme/appTheme';
+import { Text } from 'react-native';
+
 
 const Tab = createMaterialTopTabNavigator();
 
 const TopTapNavigator = () => {
-  return (
-    <Tab.Navigator>
-      <Tab.Screen name="ContactsScreen" component={ContactsScreen} />
-      <Tab.Screen name="ChatsScreen" component={ChatsScreen} />
-      <Tab.Screen name="AlbumScreen" component={AlbumScreen} />
-    </Tab.Navigator>
-  );
+
+    const { top } = useSafeAreaInsets(); 
+    
+    return (
+        <Tab.Navigator
+            style={{
+                paddingTop:top
+            }}
+            //change background color del cuerpo del componente
+            sceneContainerStyle={{
+                backgroundColor:'white'
+            }}
+            //cambio el color del press
+            screenOptions={({route}) => ({
+                tabBarPressColor:colores.primary,
+                tabBarShowIcon:true,
+                tabBarIndicatorStyle:{
+                    backgroundColor:colores.primary,
+                },
+                tabBarStyle:{
+                    borderTopWidth: 0,
+                    elevation:0,
+                    shadowColor:'transparent'
+                },
+                tabBarIcon:({color, focused})=>{
+                  let iconName : string = '';
+                  switch(route.name){
+                    case 'ContactsScreen':
+                      iconName = 'Co';
+                      break;
+                    case 'ChatsScreen':
+                      iconName = 'Cl';
+                      break;
+                    case 'AlbumScreen':
+                      iconName = 'Al';
+                      break;
+                  }
+                  return(
+                  <Text style={{color }}>
+                    {iconName}
+                  </Text>
+                  )}
+                })}
+        >
+            <Tab.Screen name="ContactsScreen" component={ContactsScreen} />
+            <Tab.Screen name="ChatsScreen" component={ChatsScreen} />
+            <Tab.Screen name="AlbumScreen" component={AlbumScreen} />
+        </Tab.Navigator>
+    );
 }
 
 export default TopTapNavigator
