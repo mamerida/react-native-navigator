41568e6f9578f7eab5f98dab6092552e8d610749
mamerida <mamerida2018@gmail.com>
2023-09-29 18:08:30 -0300

    add icons

--- src/screens/Tab1Screen.tsx
+++ src/screens/Tab1Screen.tsx
@@ -1,10 +1,14 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { styles } from '../theme/appTheme'
+import Icon from 'react-native-vector-icons/Ionicons';
 
 const Tab1Screen = () => {
+
   return (
-    <View>
-      <Text>Tab1Screen</Text>
+    <View style={styles.marginGlobal}>
+      <Text style={styles.title}>Tab1Screen</Text>
+      <Icon name="airplane-outline" size={30} color="#900" />
     </View>
   )
 }
